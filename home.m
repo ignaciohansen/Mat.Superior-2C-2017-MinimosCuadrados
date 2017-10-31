d5f885555e6e2a25fb2642262c2d94bacab6e116
Manuel da Silva Evora <manudse10@gmail.com>
2017-10-31 12:39:19 -0300

    Aproximacion Hiperbola + grafico. Falta Testear

--- home.m
+++ home.m
@@ -10,7 +10,7 @@ prompt_valores = {"Cantidad de decimales","Ingrese el nombre del archivo .xsl"};
 rowscols = [1;1.10];
 
 %TODO: Valores hardcodeados
-defaults = {"2","pruebaPotencial.xlsx"};
+defaults = {"2","pruebaHiperbola.xlsx"};
 
   while (1)
   opcion_menu_principal = menuPrincipal();
@@ -114,6 +114,28 @@ defaults = {"2","pruebaPotencial.xlsx"};
                      endwhile
                  
                case 5 %Aproximacion-Hiperbola
+                 
+                 resultado_hiperbola = aproximacionHiperbola(cantidad_decimales,matriz_puntos);
+                      while(1)
+                        opcion_menu_aproximacion = menuAproximacion();
+                        
+                        switch(opcion_menu_aproximacion)
+                            case 1 %Funcion-Aproximante
+                                msgbox(pritty_print_hiperbola(resultado_potencial));
+                                
+                            case 2 %Detalle Tabla
+                            case 3 %Grafico
+                                
+                                x= matriz_puntos(:,1);
+                                y0= matriz_puntos(:,2);
+                                y1=resultado_hiperbola(4:end);
+                                graficar(x,y0,y1,"Hiperbola");
+                                
+                            case 4 break;
+                            otherwise break;
+                        endswitch
+                     endwhile
+                 
                  
                case 6 break;
                otherwise break;
---
+++ pritty_print_hiperbola.m
@@ -0,0 +1,10 @@
+## Copyright (C) 2017 Manu
+
+
+function [retval] = pritty_print_hiperbola (input1, input2)
+RETVAL = strcat("p(x)= ", 
+                num2str(resul(2)),
+                "/(", 
+                num2str(resul(2)),
+                " + x)");
+endfunction
